21f0d7c1df2142549c8e88a91f5ae04e0bcda198
chan999 <cksrb8702@gmail.com>
2024-05-24 09:53:12 +0900

    채팅 프로그램 테스트 브랜치 채팅 프ë서비스 테스트 진행

--- src/main/java/com/ezen/springmvc/web/chat/controller/ChatController.java
+++ src/main/java/com/ezen/springmvc/web/chat/controller/ChatController.java
@@ -1,17 +1,23 @@
 package com.ezen.springmvc.web.chat.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import com.ezen.springmvc.domain.chat.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
-@RequestMapping("/chat")
-@Slf4j
 public class ChatController {
 
-    @GetMapping
-    public String chatting(){
-        return "/chat/chatting";
+    private final SimpMessageSendingOperations messagingTemplate;
+
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
---
+++ src/main/java/com/ezen/springmvc/web/chat/controller/ChatRoomController.java
@@ -0,0 +1,49 @@
+package com.ezen.springmvc.web.chat.controller;
+
+import com.ezen.springmvc.domain.chat.dto.ChatRoom;
+import com.ezen.springmvc.domain.chat.repo.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/chat")
+public class ChatRoomController {
+
+    @Autowired
+    private final ChatRoomRepository chatRoomRepository;
+
+    @GetMapping
+    public String rooms(Model model) {
+        return "/chat/room";
+    }
+
+    @GetMapping("/rooms")
+    @ResponseBody
+    public List<ChatRoom> room() {
+        return chatRoomRepository.findAllRoom();
+    }
+
+    @PostMapping("/room")
+    @ResponseBody
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatRoomRepository.createChatRoom(name);
+    }
+
+    @GetMapping("/room/enter/{roomId}")
+    public String roomDetail(Model model, @PathVariable String roomId) {
+        model.addAttribute("roomId", roomId);
+        return "/chat/roomdetail";
+    }
+
+    @GetMapping("/room/{roomId}")
+    @ResponseBody
+    public ChatRoom roomInfo(@PathVariable String roomId) {
+        return chatRoomRepository.findRoomById(roomId);
+    }
+}
