9175ad6f65fb8a94c451476d19ae816ae4d03bc1
chan999 <cksrb8702@gmail.com>
2024-05-31 08:59:18 +0900

    메채팅방 DB저장 테스트

--- src/main/java/com/ezen/springmvc/web/chat/controller/ChatRoomController.java
+++ src/main/java/com/ezen/springmvc/web/chat/controller/ChatRoomController.java
@@ -10,6 +10,8 @@ import com.ezen.springmvc.web.chat.form.MessageForm;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import oracle.net.ns.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.Timestamp;
 import java.time.Instant;
@@ -28,6 +31,7 @@ import java.util.UUID;
 @RequestMapping("/chat")
 public class ChatRoomController {
 
+    private static final Logger log = LoggerFactory.getLogger(ChatRoomController.class);
     @Autowired
     private final ChatRoomRepository chatRoomRepository;
 
@@ -55,19 +59,24 @@ public class ChatRoomController {
     }
 
     @GetMapping("/room/enter/{roomId}")
-    public String roomDetail(Model model, @PathVariable String roomId) {
-        model.addAttribute("roomId", roomId);
-        return "/chat/chatting";
+    public String roomDetail(RedirectAttributes redirectAttributes, @PathVariable String roomId) {
+        log.info(" 대화방 입장 : {}", roomId);
+        List<MessageDto> messages = chatService.getMessagesByRoomId(roomId);
+        for (MessageDto message : messages) {
+            log.info("메세지 {}", message.getContent());
+        }
+        redirectAttributes.addFlashAttribute("messages", messages);
+        return "redirect:/chat";
     }
 
-    @GetMapping("/room/{roomId}")
-    @ResponseBody
-    public ChatDto roomInfo(@PathVariable String roomId) {
-        return chatRoomRepository.findRoomById(roomId);
-    }
+//    @GetMapping("/room/{roomId}")
+//    @ResponseBody
+//    public ChatDto roomInfo(@PathVariable String roomId) {
+//        return chatRoomRepository.findRoomById(roomId);
+//    }
 
     @PostMapping("/sendMessage/{roomId}")
-    public ResponseEntity<Resource> sendMessage(@PathVariable("roomId") String roomId, @ModelAttribute("messageForm") MessageForm messageForm){
+    public ResponseEntity<Resource> sendMessage(@PathVariable("roomId") String roomId, @ModelAttribute("messageForm") MessageForm messageForm) {
         // sentAt에 현재시간 설정
 
         ChatDto chatDto = chatService.getRoom(roomId);
@@ -84,18 +93,20 @@ public class ChatRoomController {
     }
 
     @GetMapping("/room/{nickname}")
-    public String createRoomEx(@PathVariable("nickname") String nickname, HttpSession session){
+    public String createRoomEx(@PathVariable("nickname") String nickname, HttpSession session) {
 
         MemberDto loginMember = (MemberDto) session.getAttribute("loginMember");
+
+        log.info("로그인 정보는 {}", loginMember.toString());
         String roomId = UUID.randomUUID().toString();
 
         ChatDto chatDto = ChatDto.builder()
-                .senderId(loginMember.getNickname())
+                .senderId(loginMember.getMemberId())
                 .receiverId(nickname)
                 .roomId(roomId)
                 .build();
 
         chatService.newChat(chatDto);
-        return "/chat";
+        return "redirect:/chat";
     }
 }
